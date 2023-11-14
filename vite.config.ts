7c5cac0dd012917d70a7de310b24696ee12fea0a
Logan Woolf <loganwoolf@gmail.com>
2023-11-13 19:54:57 -0700

    Format files using Biome defaults

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-    build: {
-        copyPublicDir: false,
-        lib: {
-            entry: resolve(__dirname, "lib/main.ts"),
-            name: "smooth-tab",
-            formats: ["es", "umd"],
-        },
-    },
-    plugins: [dts({ include: ["lib"] })],
+	build: {
+		copyPublicDir: false,
+		lib: {
+			entry: resolve(__dirname, "lib/main.ts"),
+			name: "smooth-tab",
+			formats: ["es", "umd"],
+		},
+	},
+	plugins: [dts({ include: ["lib"] })],
 });
