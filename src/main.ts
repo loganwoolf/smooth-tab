73afc797e862aaab747b43d218a418ff6bd3b406
Logan Woolf <loganwoolf@gmail.com>
2025-05-09 02:43:07 -0600

    Enhance configuration and formatting: add CSS linter/formatter, update VSCode settings, and remove stylelint config

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,5 @@ import SmoothTab from "../lib/main";
 
 const smoothTab = new SmoothTab();
 smoothTab.init();
+
+console.log("init");
--- stylelint.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('stylelint').Config} */
-export default {
-	extends: ["stylelint-config-standard", "stylelint-config-clean-order"],
-	plugins: ["@stylistic/stylelint-plugin"],
-	rules: {
-		"@stylistic/indentation": "tab",
-	},
-};
